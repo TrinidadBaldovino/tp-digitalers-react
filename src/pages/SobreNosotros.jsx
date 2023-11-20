6a21ac7ca1123ae837593ca934fc44ff3dda684c
TrinidadBaldovino <baldovino.trinidad@gmail.com>
2023-11-20 16:53:11 -0300

    3

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -21,10 +21,10 @@ const SobreNosotros = () => {
             <div className=" mb-3">
               <div className="nosotros-iconos ">
                 <ul>
-                  <li><a href="https://www.facebook.com/onix.cel.39/"><i className="fa-brands fa-facebook-square fa-3x fb dark-icon"></i></a></li>
-                  <li><a href="https://www.instagram.com/onix_cel/"><i className="fa-brands fa-instagram-square fa-3x dark-icon"></i></a></li>
-                  <li><a href="mailto:trini@gmail.com"><i className="fa fa-envelope-square fa-3x dark-icon"></i></a></li>
-                  <li><a href="https://www.tiktok.com/@onix.cel"><i className="fa-brands fa-tiktok fa-3x dark-icon"></i></a></li>
+                  <li><a href="https://www.facebook.com/onix.cel.39/"><i className="fa-brands fa-facebook-square fa-3x fb dark-icon light-icon"></i></a></li>
+                  <li><a href="https://www.instagram.com/onix_cel/"><i className="fa-brands fa-instagram-square fa-3x dark-icon icon-light"></i></a></li>
+                  <li><a href="mailto:trini@gmail.com"><i className="fa fa-envelope-square fa-3x dark-icon icon-light"></i></a></li>
+                  <li><a href="https://www.tiktok.com/@onix.cel"><i className="fa-brands fa-tiktok fa-3x dark-icon icon-light"></i></a></li>
                 </ul>
               </div>
             </div>
